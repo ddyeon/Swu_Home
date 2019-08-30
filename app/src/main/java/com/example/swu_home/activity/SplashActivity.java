befe0ba2d04b6821f51cf9f07da79ab79d68211e
eunbiCho <puppy101976@naver.com>
2019-08-30 18:32:51 +0900

    sos

--- app/src/main/java/com/example/swu_home/activity/SplashActivity.java
+++ app/src/main/java/com/example/swu_home/activity/SplashActivity.java
@@ -30,7 +30,7 @@ public class SplashActivity extends Activity {
         }
     }
 
-    @Override
+    @Override //
     public void onBackPressed() {
         //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
     }
