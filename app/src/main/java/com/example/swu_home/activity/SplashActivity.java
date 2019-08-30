9413844cfcef4dc847a66e2d0b3707a8e85df066
eunbiCho <puppy101976@naver.com>
2019-08-30 18:23:11 +0900

    Merge branch 'master' of C:\Users\HP\AndroidStudioProjects\Swu_Home with conflicts.

--- app/src/main/java/com/example/swu_home/activity/SplashActivity.java
+++ app/src/main/java/com/example/swu_home/activity/SplashActivity.java
@@ -1,39 +1,39 @@
 package com.example.swu_home.activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.example.swu_home.R;
+import com.example.swu_home.activity.MainActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         Handler hd = new Handler();
-        hd.postDelayed(new splashhandler(), 3000);
+        hd.postDelayed(new splashhandler(), 3000); // 1초 후에 hd handler 실행  3000ms = 3초
+
     }
 
-    private class splashhandler implements Runnable {
+    private class splashhandler implements Runnable{
         public void run(){
-            startActivity(new Intent(getApplication(), MainActivity.class));
-            SplashActivity.this.finish();
+            startActivity(new Intent(getApplication(), MainActivity.class)); //로딩이 끝난 후, ChoiceFunction 이동
+            SplashActivity.this.finish(); // 로딩페이지 Activity stack에서 제거
         }
-    }*/
+    }
 
     @Override
     public void onBackPressed() {
         //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
     }
+
 }
+
